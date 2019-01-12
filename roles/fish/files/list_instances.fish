e3d0aa7b3f5c09335c2d5f451bd871e01a4f8cf4
Kyle Leaders <kyle.leaders@gmail.com>
2019-01-11 21:25:51 -0800

    Fixing list_instances

--- roles/fish/files/list_instances.fish
+++ roles/fish/files/list_instances.fish
@@ -1,3 +1,6 @@
 function list_instances
-  aws ec2 describe-instances --query 'Reservations[*].Instances[0].{ID:InstanceId,Name:Tags[?Key==`Name`].Value | [0],IP:PublicIpAddress,AMI:ImageId,Type:InstanceType}' --output table --color=off
+  aws ec2 describe-instances \
+    --query 'Reservations[*].Instances[0].{ID:InstanceId,Name:Tags[?Key==`Name`].Value | [0],IP:PublicIpAddress,AMI:ImageId,Type:InstanceType,Status:State.Name}' \
+    --output table \
+    --color=off
 end
