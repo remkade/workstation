2b079f693c25ea61bdb91f0792f9073b8d291e3a
Kyle Leaders <kyle.leaders@gmail.com>
2020-01-16 17:16:53 +0000

    Adding launch time to list_instances

--- roles/fish/files/list_instances.fish
+++ roles/fish/files/list_instances.fish
@@ -1,6 +1,14 @@
 function list_instances
-  aws ec2 describe-instances \
-    --query 'Reservations[*].Instances[0].{ID:InstanceId,Name:Tags[?Key==`Name`].Value | [0],IP:PublicIpAddress,AMI:ImageId,Type:InstanceType,Status:State.Name}' \
-    --output table \
-    --color=off
+	aws ec2 describe-instances \
+		--query 'Reservations[*].Instances[0].{
+			ID:InstanceId,
+			Name:Tags[?Key==`Name`].Value | [0],
+			IP:PublicIpAddress,
+			AMI:ImageId,
+			Type:InstanceType,
+			Status:State.Name,
+			Launched:LaunchTime
+		}' \
+		--output table \
+		--color=off
 end
