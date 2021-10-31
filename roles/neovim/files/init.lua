4bc668514907b9fed8bf6983789f7da7842d010a
Kyle Leaders <remkade@gmail.com>
2021-10-31 15:11:02 -0700

    lua works mostly now

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -1,13 +1,43 @@
 vim.g.mapleader=","
+vim.opt.termguicolors = true
 
 require('plugins')
 require('lsp')
 
-local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
 local opts = { noremap=true, silent=true }
-buf_set_keymap('n', '<leader>ff', '<Cmd>lua require("telescope.builtin").find_files()<cr>', opts)
-buf_set_keymap('n', '<leader>fg', '<Cmd>lua require("telescope.builtin").live_grep()<cr>', opts)
-buf_set_keymap('n', '<leader>fb', '<Cmd>lua require("telescope.builtin").buffers()<cr>', opts)
-buf_set_keymap('n', '<leader>fh', '<Cmd>lua require("telescope.builtin").help_tags()<cr>', opts)
+vim.api.nvim_set_keymap('n', '<leader>ff', '<Cmd>lua require("telescope.builtin").find_files()<cr>', opts)
+vim.api.nvim_set_keymap('n', '<leader>fg', '<Cmd>lua require("telescope.builtin").live_grep()<cr>', opts)
+vim.api.nvim_set_keymap('n', '<leader>fb', '<Cmd>lua require("telescope.builtin").buffers()<cr>', opts)
+vim.api.nvim_set_keymap('n', '<leader>fh', '<Cmd>lua require("telescope.builtin").help_tags()<cr>', opts)
+vim.api.nvim_set_keymap('n', '<leader>fr', '<Cmd>lua require("telescope.builtin").lsp_references()<cr>', opts)
+vim.api.nvim_set_keymap('n', '<leader>fd', '<Cmd>lua require("telescope.builtin").lsp_definitions()<cr>', opts)
+vim.api.nvim_set_keymap('n', '<leader>fe', '<Cmd>lua require("telescope.builtin").lsp_workspace_diagnostics()<cr>', opts)
+
+function randomObjectFromTable(t)
+	return t[math.random(1, #t)]
+end
+
+local schemes = {
+	'OceanicNext',
+	'atomified',
+	'blayu',
+	'candid',
+	'cryslominsa',
+	'deus',
+	'duoduo',
+	'edge',
+	'flattown',
+	'focuspoint',
+	'hybrid',
+	'nord',
+	'onenord',
+	'plastic',
+	'sonokai',
+	'spring-night',
+	'wallaby',
+        'forest-night'
+}
+
+local scheme = randomObjectFromTable(schemes)
+
+vim.cmd('colorscheme ' .. scheme)
--- roles/neovim/files/plugins.lua
+++ roles/neovim/files/plugins.lua
@@ -1,57 +1,78 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
-                                  install_path})
+	packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
+	install_path})
 end
 
 return require('packer').startup(function()
-    -- My plugins here
-    use 'rmehri01/onenord.nvim'
-    use 'L3MON4D3/LuaSnip'
+	-- Colorschemes
+	use 'rmehri01/onenord.nvim'
+	use 'Siphalor/vim-atomified'
+	use 'Yggdroot/duoduo'
+	use 'ajmwagar/vim-deus'
+	use 'arcticicestudio/nord-vim'
+	use 'blerins/flattown'
+	use 'chase/focuspoint-vim'
+	use 'flrnd/candid.vim'
+	use 'flrnprz/plastic.vim'
+	use 'lithammer/vim-eighties'
+	use 'mhartington/oceanic-next'
+	use 'rhysd/vim-color-spring-night'
+	use 'rhysd/wallaby.vim'
+	use 'sainnhe/archived-colors'
+	use 'sainnhe/edge'
+	use 'sainnhe/sonokai'
+	use 'sainnhe/vim-color-forest-night'
+	use 'scwood/vim-hybrid'
+	use 'tjammer/blayu.vim'
+	use 'L3MON4D3/LuaSnip'
 
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = {
-		{'nvim-lua/plenary.nvim'},
-		{'nvim-telescope/telescope-fzy-native.nvim'}
-	},
-	config = function() require('telescope').load_extension('fzy_native') end,
-    }
+	-- Icons
+	use 'kyazdani42/nvim-web-devicons'
 
-    use {
-        'TimUntersberger/neogit',
-        requires = 'nvim-lua/plenary.nvim'
-    }
+	use {
+		'nvim-telescope/telescope.nvim',
+		requires = {
+			{'nvim-lua/plenary.nvim'},
+			{'nvim-telescope/telescope-fzy-native.nvim'}
+		},
+		config = function() require('telescope').load_extension('fzy_native') end,
+	}
 
-    use 'neovim/nvim-lspconfig'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/nvim-cmp'
+	use {
+		'TimUntersberger/neogit',
+		requires = 'nvim-lua/plenary.nvim'
+	}
 
-    -- For vsnip users.
-    use 'hrsh7th/cmp-vsnip'
-    use 'hrsh7th/vim-vsnip'
+	use 'neovim/nvim-lspconfig'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use 'hrsh7th/nvim-cmp'
 
-    vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
-    ]])
+	-- For vsnip users.
+	use 'hrsh7th/cmp-vsnip'
+	use 'hrsh7th/vim-vsnip'
 
-    use {
-        "folke/trouble.nvim",
-        requires = "kyazdani42/nvim-web-devicons"
-    }
+	vim.cmd([[
+	augroup packer_user_config
+	autocmd!
+	autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+	augroup end
+	]])
 
-    use {"folke/which-key.nvim"}
+	use {
+		"folke/trouble.nvim",
+		requires = "kyazdani42/nvim-web-devicons"
+	}
 
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
-    if packer_bootstrap then
-        require('packer').sync()
-    end
+	use {"folke/which-key.nvim"}
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
