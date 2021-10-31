3fcbbf4aed66cdb399b25def7d3d12a3eb8a67a0
Kyle Leaders <remkade@gmail.com>
2021-10-30 21:45:11 -0700

    Initial lua stuff

---
+++ roles/neovim/files/plugins.lua
@@ -0,0 +1,7 @@
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Simple plugins can be specified as strings
+  use '9mm/vim-closer'
+end)
