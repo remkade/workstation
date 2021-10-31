13c9727d81979294555ab1158e5a1da4a74f291a
Kyle Leaders <remkade@gmail.com>
2021-10-30 22:13:41 -0700

    Starting with plugins

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -1,2 +1,8 @@
 require('plugins')
 require('lsp')
+
+local opts = { noremap=true, silent=true }
+buf_set_keymap('n', '<leader>ff', '<Cmd>lua require("telescope.builtin").find_files()<cr>', opts)
+buf_set_keymap('n', '<leader>fg', '<Cmd>lua require("telescope.builtin").live_grep()<cr>', opts)
+buf_set_keymap('n', '<leader>fb', '<Cmd>lua require("telescope.builtin").buffers()<cr>', opts)
+buf_set_keymap('n', '<leader>fh', '<Cmd>lua require("telescope.builtin").help_tags()<cr>', opts)
