1e52372a7d2c3b5d64f936cb025d04fb0f6cd1d5
Kyle Leaders <kleaders@anaconda.com>
2022-08-30 15:30:55 -0700

    Removing neorg

--- roles/neovim/files/plugins.lua
+++ roles/neovim/files/plugins.lua
@@ -151,28 +151,6 @@ return require('packer').startup(function()
 		setup = function() require('feline_ibhagwan') end,
 	}
 
-	-- Neorg
-	use { 
-		"nvim-neorg/neorg",
-		config = function()
-			require('neorg').setup {
-				-- Tell Neorg what modules to load
-				load = {
-					["core.defaults"] = {}, -- Load all the default modules
-					["core.norg.concealer"] = {}, -- Allows for use of icons
-					["core.norg.dirman"] = { -- Manage your directories with Neorg
-					config = {
-						workspaces = {
-							my_workspace = "~/neorg"
-						}
-					}
-				}
-			},
-		}
-		end,
-		requires = "nvim-lua/plenary.nvim"
-	}
-
 	-- Shell Check
 	use 'itspriddle/vim-shellcheck'
 
