a31f4f9a7ea23067de70b220ce2f140c93190268
Kyle Leaders <kyle.leaders@gmail.com>
2026-02-06 10:10:24 -0800

    Fixing missing commas

--- roles/neovim/files/plugins.lua
+++ roles/neovim/files/plugins.lua
@@ -115,7 +115,7 @@ plugins = {
 			-- If you want the formatexpr, here is the place to set it
 			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 		end,
-	}
+	},
 
 	-- LSP and completion
 	{
--- roles/neovim/files/treesitter.lua
+++ roles/neovim/files/treesitter.lua
@@ -13,9 +13,9 @@ require'nvim-treesitter.configs'.setup {
 
 			keymaps = {
 				-- You can use the capture groups defined in textobjects.scm
-				["af"] = { query = "@function.outer", desc = "Select around function" }
-				["if"] = { query = "@function.inner", desc = "Select inside function" }
-				["ac"] = { query = "@class.outer", desc = "Select around class" }
+				["af"] = { query = "@function.outer", desc = "Select around function" },
+				["if"] = { query = "@function.inner", desc = "Select inside function" },
+				["ac"] = { query = "@class.outer", desc = "Select around class" },
 				["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
 				-- You can also use captures from other query groups like `locals.scm`
 				["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
